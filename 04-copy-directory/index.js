58a717c8eced318efd7d75419e326eba335051e9
Nargiza14 <ruzieva.aya@icloud.com>
2023-05-08 01:21:05 +0600

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(
+  path.join(__dirname + "/files-copy"),
+  { recursive: true },
+  function copyDir(err) {
+    if (err) throw err;
+    console.log("Clone folder");
+    fs.readdir(path.join(__dirname + "/files"), "utf8", function (error, data) {
+      for (const key in data) {
+        fs.copyFile(
+          path.join(__dirname + "/files" + "/" + data[key]),
+          path.join(__dirname + "/files-copy" + "/" + data[key]),
+          (err) => {
+            if (err) throw err;
+            console.log("Done!");
+          }
+        );
+      }
+    });
+  }
+);
