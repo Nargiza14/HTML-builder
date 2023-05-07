fcc9c66e1d8f64fae6563a1f52bb2e0db6e18942
Nargiza14 <ruzieva.aya@icloud.com>
2023-05-08 00:00:14 +0600

    feat: add 03-files-in-folder task solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,13 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.promises
-  .readdir(path.join(__dirname + `/secret-folder`), { withFileTypes: true })
-  .then((filenames) => {
-    for (let filename of filenames) {
-      console.log(filename);
+fs.readdir(
+  path.join(__dirname + `/secret-folder`),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        if (!file.isDirectory()) {
+          fs.stat(path.join(__dirname + `/secret-folder`), (error, stats) => {
+            if (error) {
+              console.log(error);
+            } else {
+              let name = file.name.split(".", 1).toString();
+              let extension = path
+                .extname(path.join(__dirname + `/secret-folder`) + file.name)
+                .replace(".", "");
+              let size = stats.size + "b";
+              console.log(name + "-" + extension + "-" + size);
+            }
+          });
+        }
+      });
     }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  }
+);
