294720c73a36a8a7fc545e170e7c84acfa27a7b9
Nargiza14 <ruzieva.aya@icloud.com>
2023-05-09 01:13:41 +0600

    fix: change 03-files-in-folder solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,18 +9,21 @@ fs.readdir(
     else {
       files.forEach((file) => {
         if (!file.isDirectory()) {
-          fs.stat(path.join(__dirname + `/secret-folder`), (error, stats) => {
-            if (error) {
-              console.log(error);
-            } else {
-              let name = file.name.split(".", 1).toString();
-              let extension = path
-                .extname(path.join(__dirname + `/secret-folder`) + file.name)
-                .replace(".", "");
-              let size = stats.size + "b";
-              console.log(name + "-" + extension + "-" + size);
+          fs.stat(
+            path.join(__dirname + `/secret-folder`, file.name),
+            (error, stats) => {
+              if (error) {
+                console.log(error);
+              } else {
+                let name = file.name.split(".").toString().replace(",", "-");
+                let extension = path
+                  .extname(path.join(__dirname + `/secret-folder`), file.name)
+                  .replace(".", "");
+                let size = stats.size + "b";
+                console.log(name + "-" + extension + size);
+              }
             }
-          });
+          );
         }
       });
     }
