9849c01d60697cbdb588b0d8467057db577a1203
Nargiza14 <ruzieva.aya@icloud.com>
2023-05-11 05:00:36 +0600

    fix: change 05-merge-styles solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,30 +1,17 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
-
+const { createWriteStream, createReadStream } = require("fs");
 const newFile = path.join(__dirname + "/project-dist" + "/bundle.css");
+const style = path.join(__dirname + "/styles");
 
-fs.readdir(
-  path.join(__dirname + "/styles"),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) console.log(err);
-    else {
-      files.forEach((file) => {
-        const extension = path.extname(
-          path.join(__dirname + "/styles") + file.name
-        );
-        if (extension == ".css") {
-          const cssFiles = new fs.ReadStream(
-            path.join(__dirname + "/styles" + "/" + file.name)
-          );
-          cssFiles.on("data", (el) => {
-            fs.appendFile(newFile, el.toString(), function (error) {
-              if (error) throw error;
-              console.log("Done!");
-            });
-          });
-        }
-      });
+async function addStyles() {
+  const add = createWriteStream(newFile, "utf8");
+  const parts = await fs.readdir(style, { withFileTypes: true });
+
+  for (part of parts) {
+    if (part.isFile() && path.extname(part.name) === ".css") {
+      createReadStream(path.join(style, part.name), "utf8").pipe(add);
     }
   }
-);
+}
+addStyles();
